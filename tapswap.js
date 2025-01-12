da7ed737a6903b0a76d27f233ccf63416b4a0678
thanhturau <thanhturau@gmail.com>
2025-01-12 20:22:30 +0700

    Update tapswap.js

--- tapswap.js
+++ tapswap.js
@@ -1,11 +1,426 @@
-
 [
     {
+        "Q": "How Blockchain, IoT & AI Are Changing the World. Part 1  🦾 | TapSwap Education",
+        "A": "A1"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 9 | TapSwap Education",
+        "A": "A2"
+    },
+    {
+        "Q": "Pepe Coin Explained | Part 2",
+        "A": "A3"
+    },
+    {
+        "Q": "Leverage LinkedIn",
+        "A": "A4"
+    },
+    {
+        "Q": "Profitable Clothing Line",
+        "A": "A5"
+    },
+    {
+        "Q": "Create and Monetize",
+        "A": "A6"
+    },
+    {
+        "Q": "Tips for New Entrepreneurs",
+        "A": "A7"
+    },
+    {
         "Q": "From Beginner to Expert",
-		"A": "4bvr"
+        "A": "A8"
     },
     {
         "Q": "High CPM in 2024-2025",
-		"A": "hd27"
+        "A": "A9"
+    },
+    {
+        "Q": "Profit from Your Expertise",
+        "A": "A10"
+    },
+    {
+        "Q": "Profitable Franchises",
+        "A": "A11"
+    },
+    {
+        "Q": "AI-Driven Dropshipping",
+        "A": "A12"
+    },
+    {
+        "Q": "Powerful Lessons",
+        "A": "A13"
+    },
+    {
+        "Q": "Stunning Portfolio",
+        "A": "A14"
+    },
+    {
+        "Q": "Creating a Website",
+        "A": "A15"
+    },
+    {
+        "Q": "Life-Changing",
+        "A": "A16"
+    },
+    {
+        "Q": "Minimalist Principles",
+        "A": "A17"
+    },
+    {
+        "Q": "Must-Have AI Apps",
+        "A": "A18"
+    },
+    {
+        "Q": "Maximize Your Small Business",
+        "A": "A19"
+    },
+    {
+        "Q": "Crack the Code",
+        "A": "A20"
+    },
+    {
+        "Q": "Passive Income",
+        "A": "A21"
+    },
+    {
+        "Q": "LinkedIn Success",
+        "A": "A22"
+    },
+    {
+        "Q": "Outsource Without Breaking the Bank",
+        "A": "A23"
+    },
+    {
+        "Q": "Google Search Hacks",
+        "A": "A24"
+    },
+    {
+        "Q": "Student Entrepreneurs",
+        "A": "A25"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 8 | TapSwap Education",
+        "A": "A26"
+    },
+    {
+        "Q": "Budget Travel Secrets",
+        "A": "A27"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 7 | TapSwap Education",
+        "A": "A28"
+    },
+    {
+        "Q": "Secure Your Future",
+        "A": "A29"
+    },
+    {
+        "Q": "Boost Your Productivity",
+        "A": "A30"
+    },
+    {
+        "Q": "Pepe Coin Explained | Part 1",
+        "A": "A31"
+    },
+    {
+        "Q": "From Setup to Sales",
+        "A": "A32"
+    },
+    {
+        "Q": "Own Fashion Brand",
+        "A": "A33"
+    },
+    {
+        "Q": "Successful Blog",
+        "A": "A34"
+    },
+    {
+        "Q": "Zero-Waste Product Line",
+        "A": "A35"
+    },
+    {
+        "Q": "Professional Problem Solver",
+        "A": "A36"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 6 | TapSwap Education",
+        "A": "A37"
+    },
+    {
+        "Q": "YouTube Niches",
+        "A": "A38"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 5 | TapSwap Education",
+        "A": "A39"
+    },
+    {
+        "Q": "Monetize Your Expertise",
+        "A": "A40"
+    },
+    {
+        "Q": "Low-Cost Franchises",
+        "A": "A41"
+    },
+    {
+        "Q": "💥 How a $1,500 Dogecoin Investment Grew 1,000%! |Part 5",
+        "A": "A42"
+    },
+    {
+        "Q": "Secret Formula",
+        "A": "A43"
+    },
+    {
+        "Q": "Life Lessons",
+        "A": "A44"
+    },
+    {
+        "Q": "Irresistible Online Portfolio",
+        "A": "A45"
+    },
+    {
+        "Q": "Professional Website",
+        "A": "A46"
+    },
+    {
+        "Q": "Financial Goals",
+        "A": "A47"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 4 | TapSwap Education",
+        "A": "A48"
+    },
+    {
+        "Q": "Created a Budget",
+        "A": "A49"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 3 | TapSwap Education",
+        "A": "A50"
+    },
+    {
+        "Q": "Unlock Your Productivity",
+        "A": "A51"
+    },
+    {
+        "Q": "Winning Marketing Strategy",
+        "A": "A52"
+    },
+    {
+        "Q": "🚀 Dogecoin Price Predictions | Part 4",
+        "A": "A53"
+    },
+    {
+        "Q": "Seasonal Products",
+        "A": "A54"
+    },
+    {
+        "Q": "Hidden Profits",
+        "A": "A55"
+    },
+    {
+        "Q": "Unlock Career Opportunities",
+        "A": "A56"
+    },
+    {
+        "Q": "Outsource Like a Boss",
+        "A": "A57"
+    },
+    {
+        "Q": "Making $500/Day",
+        "A": "A58"
+    },
+    {
+        "Q": "Cyberattacks Explained | 🛡 Part 2 | TapSwap Education",
+        "A": "A59"
+    },
+    {
+        "Q": "Be Your Own Boss",
+        "A": "A60"
+    },
+    {
+        "Q": "Cyberattacks Explained | Part 1 | TapSwap Education",
+        "A": "A61"
+    },
+    {
+        "Q": "Travel the World",
+        "A": "A62"
+    },
+    {
+        "Q": "10 Mistakes to Avoid",
+        "A": "A63"
+    },
+    {
+        "Q": "🐕 Elon Musk + Celebs = Dogecoin Explosion! | Part 3",
+        "A": "A64"
+    },
+    {
+        "Q": "Maximizing Your Productivity",
+        "A": "A65"
+    },
+    {
+        "Q": "Launch Your TikTok Shop",
+        "A": "A66"
+    },
+    {
+        "Q": "Make Money Fast",
+        "A": "A67"
+    },
+    {
+        "Q": "Build a One-Person Business",
+        "A": "A68"
+    },
+    {
+        "Q": "Full Roadmap",
+        "A": "A69"
+    },
+    {
+        "Q": "Bitcoin vs. Banks | Part 4 | TapSwap Education",
+        "A": "A70"
+    },
+    {
+        "Q": "Master Negotiation",
+        "A": "A71"
+    },
+    {
+        "Q": "Bitcoin vs. Banks | Part 3 | TapSwap Education",
+        "A": "A72"
+    },
+    {
+        "Q": "Easiest Way to Start",
+        "A": "A73"
+    },
+    {
+        "Q": "Winning Team",
+        "A": "A74"
+    },
+    {
+        "Q": "💡 Dogecoin Explained | Part 2",
+        "A": "A75"
+    },
+    {
+        "Q": "From Zero to Sales",
+        "A": "A76"
+    },
+    {
+        "Q": "Zero to Hero",
+        "A": "A77"
+    },
+    {
+        "Q": "Break Free",
+        "A": "A78"
+    },
+    {
+        "Q": "From Photos to Paychecks",
+        "A": "A79"
+    },
+    {
+        "Q": "Billionaires Seal the Deal",
+        "A": "A80"
+    },
+    {
+        "Q": "Bitcoin vs. Banks | Part 2 | TapSwap Education",
+        "A": "A81"
+    },
+    {
+        "Q": "Furniture Flipping",
+        "A": "A82"
+    },
+    {
+        "Q": "Bitcoin vs. Banks | Part 1 | TapSwap Education",
+        "A": "A83"
+    },
+    {
+        "Q": "Personalized Gifts",
+        "A": "A84"
+    },
+    {
+        "Q": "Shopify Dropshipping Blueprint",
+        "A": "A85"
+    },
+    {
+        "Q": "🚀 The Story of Dogecoin | Part 1",
+        "A": "A86"
+    },
+    {
+        "Q": "Ultimate Startup Team",
+        "A": "A87"
+    },
+    {
+        "Q": "From 9-to-5 to Financial Freedom",
+        "A": "A88"
+    },
+    {
+        "Q": "Laser Engraving",
+        "A": "A89"
+    },
+    {
+        "Q": "Solo Business",
+        "A": "A90"
+    },
+    {
+        "Q": "Antique Furniture",
+        "A": "A91"
+    },
+    {
+        "Q": "Bitcoin Mining Explained | Part 4 🚀",
+        "A": "A92"
+    },
+    {
+        "Q": "Rules Billionaires",
+        "A": "A93"
+    },
+    {
+        "Q": "Bitcoin Mining Explained | Part 3 🚀",
+        "A": "A94"
+    },
+    {
+        "Q": "Fake Paparazzi Business",
+        "A": "A95"
+    },
+    {
+        "Q": "Shopify Dropshipping Store",
+        "A": "A96"
+    },
+    {
+        "Q": "⚡ How to Create a Viral Meme Coin Using Pump.fun | Part 6",
+        "A": "A97"
+    },
+    {
+        "Q": "Escape the Rat Race",
+        "A": "A98"
+    },
+    {
+        "Q": "Perfect Team for Business",
+        "A": "A99"
+    },
+    {
+        "Q": "Flip Antique Furniture",
+        "A": "A100"
+    },
+    {
+        "Q": "Dropshipping on Shopify",
+        "A": "A101"
+    },
+    {
+        "Q": "Profitable Gift Business",
+        "A": "A102"
+    },
+    {
+        "Q": "Bitcoin Mining Explained | Part 2",
+        "A": "A103"
+    },
+    {
+        "Q": "Pretending to Be a Paparazzi",
+        "A": "A104"
+    },
+    {
+        "Q": "Bitcoin Mining Explained | Part 1",
+        "A": "A105"
+    },
+    {
+        "Q": "From Scratch to Success",
+        "A": "A106"
     }
 ]
